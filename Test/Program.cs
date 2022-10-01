00277374f7945318157124380283aabe75121f34
Alisher <kusainov-alisher@mail.ru>
2022-10-01 19:24:05 +0600

    added Test Work

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] array = {"example", "fin", "test", "5" };
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine();
+
+for (int j = 0; j < array.Length; j++)
+{
+    if(array[j].Length <= 3)
+    {
+        Console.Write($"{array[j]} ");
+    }
+}
